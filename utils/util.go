996429310ab7f527e655b3085bba79e6b0d9eafd
akhettar <ayache@openconsult.digital>
2020-04-30 17:52:22 +0300

    doc added

--- utils/util.go
+++ utils/util.go
@@ -5,6 +5,7 @@ import (
 	"os"
 )
 
+// FetchEnvVar returns environment variable if not found it will return the given default value
 func FetchEnvVar(key, defaultValue string) string {
 	value, ok := os.LookupEnv(key)
 	if ok {
@@ -17,6 +18,7 @@ func FetchEnvVar(key, defaultValue string) string {
 	return defaultValue
 }
 
+// RetrieveEnvVar returns environment and if not found it logs a fatal error
 func RetrieveEnvVar(key string) string {
 	value, ok := os.LookupEnv(key)
 	if ok {
